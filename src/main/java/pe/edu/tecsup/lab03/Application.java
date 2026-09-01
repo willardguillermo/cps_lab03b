1b38cc225a03b926fb1559458616dfd73b4fbc49
willard guillermo <willardguillermo@gmail.com>
2026-09-01 13:47:40 -0500

    primer commit proyecto lab03

---
+++ src/main/java/pe/edu/tecsup/lab03/Application.java
@@ -0,0 +1,8 @@
+package pe.edu.tecsup.lab03;
+
+public class Application {
+    public static void main (String [] args) {
+        System.out.println("Laboratorio 03 - Control de Versiones Avanzado");
+    }
+
+}
