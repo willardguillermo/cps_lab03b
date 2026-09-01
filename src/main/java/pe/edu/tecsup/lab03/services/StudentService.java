5f5d60a3f8764074beff01b27f49bfd59a12a5d1
willardguillermo <willardguillermo@gmail.com>
2026-09-01 15:54:08 -0500

    Correccion de StudentService para cantidad de estudiantes

--- src/main/java/pe/edu/tecsup/lab03/services/StudentService.java
+++ src/main/java/pe/edu/tecsup/lab03/services/StudentService.java
@@ -27,6 +27,6 @@ public class StudentService {
     }
 
     public int cantidadEstudiantes() {
-        return StudentRepository.findAll().size();
+        return studentRepository.findAll().size();
     }
 }
